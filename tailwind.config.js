5cfee24ee178d5ce1d82d746d37ba98148f519ae
Jaime Gonzalez <lnsflive@gmail.com>
2025-06-12 00:42:45 -0500

    module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
-        flip: 'flip 0.4s ease-in-out'
+        flip: "flip 0.4s ease-in-out",
       },
       keyframes: {
         flip: {
-          '0%': {transform: 'rotateX(-90deg)'},
-          '100%': {transform: 'rotateX(0deg)'}
-        }
-      }
+          "0%": { transform: "rotateX(-90deg)" },
+          "100%": { transform: "rotateX(0deg)" },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
